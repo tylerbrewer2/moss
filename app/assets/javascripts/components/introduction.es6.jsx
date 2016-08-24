2e0481006482590e753ab939e5eb35b1a9664fd4
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2016-08-24 18:53:27 -0400

    initial commit

---
+++ app/assets/javascripts/components/introduction.es6.jsx
@@ -0,0 +1,9 @@
+class Introduction extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1 className='testing'>Introduction: {this.props.introduction}</h1>
+      </div>
+    );
+  }
+}
